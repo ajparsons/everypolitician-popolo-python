c7f0caf000c444489e353d1765e3de88fbdc67fd
Alex Parsons <alex@alexparsons.co.uk>
2017-02-03 10:39:52 +0000

    Amend PopoloObject to prepare for read/write data objects
    In new system, rather than the properties of a PopoloObject
    being defined as @property-ed functions accessing properties
    of self.data - this will be replaced by the 'Attribute' class
     - that given a attribute will control getting and setting
     to self.data.
    
    Subclasses of Attribute control different ways of talking to data
    e.g. talking to data that is a date.
    
    A small meta-class is used for PopoloObject to avoid repetition
    in definitions.
    e.g. person = Property(attr=person) can be expressed as
    person = Property()

--- popolo_data/base.py
+++ popolo_data/base.py
@@ -1,29 +1,179 @@
+
 from datetime import date
-import json
-import re
-
-
 from approx_dates.models import ApproxDate
-
 import six
+import json
 
 def first(l):
     '''Return the first item of a list, or None if it's empty'''
     return l[0] if l else None
 
+class Attribute(object):
+    '''
+    Exposes raw json values for getting and setting.
+    
+    Works in conjecture with PopoloObject
+    
+    so:
+    
+    id = Attribute()
+    
+    will give a PopoloObject with an id attribute that gets and
+    sets the 'id' attr of self.data.
+    
+    id = Attribute(default="16")
+    
+    Sets a default extraction value of 16.
+    
+    id = Attribute(null=True)
+    
+    will return None rather than an error if the value is absent.
+    
+    '''
+    def __init__(self,attr="",default=None,null=False,allow_multiple=False):
+        self.attr = attr
+        self.default_value = default
+        self.allow_null_default = null
+        self.allow_multiple = allow_multiple
 
-class ObjectDoesNotExist(Exception):
-    pass
+    def __get__(self, obj, type=None):
+        if self.allow_null_default == False and self.default_value == None:
+            return obj.data.get(self.attr)
+        else:
+            return obj.data.get(self.attr,self.default_value)
+
+    def __set__(self,obj,value):
+        if isinstance(value,str):
+            nv = unicode(value)
+        else:
+            nv = value
+        
+        obj.data[self.attr] = nv
+
+class RelatedAttribute(Attribute):
+    """
+    returns 'related' objects - e.g. if person_id = 5, returns Person 5
+    """
+    def __init__(self,attr="",default=None,null=False,
+                 id_attr=None,collection=None):
+        self.attr = attr
+        self.default_value = default
+        self.allow_null_default = null
+        self._id_attr = id_attr
+        self._collection = collection
+            
+    @property
+    def id_attr(self):
+        if self._id_attr:
+            return self._id_attr
+        else:
+            return self.attr + "_id"
+          
+    @property  
+    def collection(self):
+        if self._collection:
+            return self._collection
+        else:
+            return self.attr + "s"   
+         
+    def __get__(self, obj, type=None):
+        collection = getattr(obj.all_popolo,self.collection)
+        return collection.lookup_from_key[getattr(obj,self.id_attr)]
+    
+    def __set__(self,obj,value):
+        setattr(obj,self.id_attr,value.id)
+
+class DateAttribute(Attribute):
+    """
+    Interacts with ApproxDates - sets iso, retrieves ApproxDate
+    """
+    def __get__(self, obj, type=None):
+        if self.allow_null_default:
+            return obj.get_date(self.attr,None)
+        else:
+            return obj.get_date(self.attr,self.default_value)
+    
+    def __set__(self,obj,value):
+        if isinstance(value,ApproxDate):
+            obj.data[self.attr] = value.isoformat()
+        else:
+            obj.data[self.attr] = value
 
 
-class MultipleObjectsReturned(Exception):
-    pass
+class IdentiferAttribute(Attribute):
+    """
+    For getting and setting values deeper in linked data.
+    """
+    getter = "identifier_values"
+    
+    def __get__(self, obj, type=None):
+        getter = getattr(obj,self.__class__.getter)
+        v = getter(self.attr)
+        if v:
+            if self.allow_multiple:
+                return v
+            else:
+                return first(v)
+        elif v == None and self.allow_null_default:
+            return None
+        else:
+            return self.default_value
+        
+    def __set__(self,obj,value):
+        setter = getattr(obj,"set_" + self.__class__.getter)
+        setter(self.attr,value)
 
-class PopoloObject(object):
+class LinkAttribute(IdentiferAttribute):
+    getter = "link_values"
+    
+class ContactAttribute(IdentiferAttribute):
+    getter = "contact_detail_values"
 
-    def __init__(self, data, all_popolo):
+class PopoloMeta(type):
+    
+    def __new__(cls, name, parents, dct):
+        
+        """
+        If attr value not specified for an attribute, gives it the name assigned.
+        
+        This specifies the key of the popolo dict the property refers to.
+        
+        so 
+        
+        name = Attribute()
+        
+        is equivalent to:
+        
+        name = Attribute(attr="name")
+        """
+        
+        for k,v in six.iteritems(dct):
+            if isinstance(v,Attribute) :
+                if v.attr == "":
+                    v.attr = k 
+
+        cls = super(PopoloMeta, cls).__new__(cls, name, parents, dct)
+        return cls
+ 
+
+class PopoloObject(six.with_metaclass(PopoloMeta,object)):
+
+    class DoesNotExist(Exception):
+        pass
+
+    class MultipleObjectsReturned(Exception):
+        pass
+
+    def __init__(self, data=None, all_popolo=None,**kwargs):
+        if data == None:
+            data = {}
+        data.update(kwargs)
         self.data = data
         self.all_popolo = all_popolo
+
+    @property
+    def json(self):
+        return json.dumps(self.data)
 
     def get_date(self, attr, default):
         d = self.data.get(attr)
@@ -66,17 +216,65 @@ class PopoloObject(object):
         return [
             o[info_value_key]
             for o in self.get_related_object_list(popolo_array)
-            if o[info_type_key] == info_type]
+            if o[info_type_key] == info_type
+            ]
+
+    def del_related_values(self,popolo_array, info_type_key, info_type):
+        obj_list = self.get_related_object_list(popolo_array)
+        if obj_list:
+            for x,o in enumerate(obj_list):
+                if o[info_type_key] == info_type:
+                    break
+            
+            if obj_list[x][info_type_key] == info_type:
+                del obj_list[x]
+            
+
+    def set_related_values(self, popolo_array
+                           , info_type_key, info_type, info_value_key,new_value):
+        """
+        allows related values to be set
+        """
+        
+        obj_list = self.get_related_object_list(popolo_array)
+        for o in obj_list:
+            if o[info_type_key] == info_type:
+                o[info_value_key] = new_value
+                return
+        new = {info_type_key:info_type,
+               info_value_key:new_value}
+        obj_list.append(new)
+        self.data[popolo_array] = obj_list
 
     def identifier_values(self, scheme):
         return self.get_related_values(
             'identifiers', 'scheme', scheme, 'identifier')
+
+    def set_identifier_values(self, scheme,value):
+        """
+        set an identifer value
+        """
+        self.set_related_values(
+            'identifiers', 'scheme', scheme, 'identifier',value)
 
     def identifier_value(self, scheme):
         return first(self.identifier_values(scheme))
 
     def link_values(self, note):
         return self.get_related_values('links', 'note', note, 'url')
+
+    def set_link_values(self, note,value):
+        """
+        set a link value
+        """
+        self.set_related_values('links', 'note', note, 'url',value)
+
+    def del_link_values(self, note):
+        """
+        set a link value
+        """
+        self.del_related_values('links', 'note', note)
+
 
     def link_value(self, note):
         return first(self.link_values(note))
@@ -85,8 +283,19 @@ class PopoloObject(object):
         return self.get_related_values(
             'contact_details', 'type', contact_type, 'value')
 
+    def del_contact_detail_values(self, contact_type):
+        return self.del_contact_detail_values(
+            'contact_details', 'type', contact_type)
+    
+    def set_contact_detail_values(self, contact_type,new_value):
+        return self.set_related_values(
+            'contact_details', 'type', contact_type, 'value',new_value)
+
     def contact_detail_value(self, contact_type):
         return first(self.contact_detail_values(contact_type))
+
+    def absorb(self,other):
+        pass
 
     @property
     def key_for_hash(self):
@@ -112,6 +321,16 @@ class PopoloObject(object):
             return fmt.format(class_name, enclosed_text.encode('utf-8'))
         return fmt.format(class_name, enclosed_text)
 
+    def __repr__(self):
+        """
+        generic __repr__ for different models - will repr with
+        whichever value is present in object.
+        """
+        preferred_order = ["name","label","id"]
+        for o in preferred_order:
+            if hasattr(self,o):
+                return self.repr_helper(getattr(self,o))
+
 
 class CurrentMixin(object):
 
@@ -122,9 +341,4 @@ class CurrentMixin(object):
     @property
     def current(self):
         return self.current_at(date.today())
-
-
-
-
-
 
