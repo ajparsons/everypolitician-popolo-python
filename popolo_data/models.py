bc36884d9fdb0d2d0f3d597ae241d664919b3c7a
Alex Parsons <alex@alexparsons.co.uk>
2017-02-03 10:59:28 +0000

    PopoloObject subclasses moved to new attribute system
    Almost all previous @property-ed functions replaced with
    Attribute object.
    
    Functionality for 'getting' and default values are identical
    to old arrangement.

--- popolo_data/models.py
+++ popolo_data/models.py
@@ -2,12 +2,19 @@
 Classes to access items inside Popolo Collections
 
 """
+
 import json
+import re
 from six.moves.urllib_parse import urlsplit
 
 from approx_dates.models import ApproxDate
 
-from .base import PopoloObject, CurrentMixin,ObjectDoesNotExist
+from .base import PopoloObject, CurrentMixin
+from .base import Attribute, IdentiferAttribute, DateAttribute, \
+                LinkAttribute, ContactAttribute, RelatedAttribute
+
+safe_property = property #we define an attribute of person as 'property'
+
 
 def _is_name_current_at(name_object, date_string):
     start_range = name_object.get('start_date') or '0001-01-01'
@@ -20,8 +27,6 @@ def extract_twitter_username(username_or_url):
         return re.sub(r'^/([^/]+).*', r'\1', split_url.path)
     return username_or_url.strip().lstrip('@')
 
-
-
 def unique_preserving_order(sequence):
     '''Return a list with only the unique elements, preserving order
 
@@ -32,78 +37,37 @@ def unique_preserving_order(sequence):
 
 
 class Person(PopoloObject):
+    id = Attribute()
+    email = Attribute()
+    gender = Attribute()
+    honorific_prefix = Attribute()
+    honorific_suffix = Attribute()
+    image = Attribute()
+    name = Attribute()
+    sort_name = Attribute()
+    national_identity = Attribute()
+    biography = Attribute()
+    birth_date = DateAttribute(default=None)
+    death_date = DateAttribute(default=None)
+    family_name = Attribute()
+    given_name = Attribute()
+    summary = Attribute()
+    wikidata = IdentiferAttribute()
+    phone = ContactAttribute()
+    fax = ContactAttribute()
+    property = ContactAttribute()
+    facebook = LinkAttribute()
+    links = Attribute(default=[])
+    contact_details = Attribute(default=[])
+    identifiers = Attribute(default=[])
+    images = Attribute(default=[])
+    other_names = Attribute(default=[])
+    sources = Attribute(default=[])
+    phone_all = ContactAttribute(attr="phone",allow_multiple=True)
+    facebook_all = LinkAttribute(attr="facebook",allow_multiple=True)
+    fax_all = ContactAttribute(attr="fax",allow_multiple=True)
 
-    class DoesNotExist(ObjectDoesNotExist):
-        pass
-
-    class MultipleObjectsReturned(MultipleObjectsReturned):
-        pass
-
-    @property
-    def id(self):
-        return self.data.get('id')
-
-    @property
-    def email(self):
-        return self.data.get('email')
-
-    @property
-    def gender(self):
-        return self.data.get('gender')
-
-    @property
-    def honorific_prefix(self):
-        return self.data.get('honorific_prefix')
-
-    @property
-    def honorific_suffix(self):
-        return self.data.get('honorific_suffix')
-
-    @property
-    def image(self):
-        return self.data.get('image')
-
-    @property
-    def name(self):
-        return self.data.get('name')
-
-    @property
-    def sort_name(self):
-        return self.data.get('sort_name')
-
-    @property
-    def national_identity(self):
-        return self.data.get('national_identity')
-
-    @property
-    def summary(self):
-        return self.data.get('summary')
-
-    @property
-    def biography(self):
-        return self.data.get('biography')
-
-    @property
-    def birth_date(self):
-        return self.get_date('birth_date', None)
-
-    @property
-    def death_date(self):
-        return self.get_date('death_date', None)
-
-    @property
-    def family_name(self):
-        return self.data.get('family_name')
-
-    @property
-    def given_name(self):
-        return self.data.get('given_name')
-
-    @property
-    def wikidata(self):
-        return self.identifier_value('wikidata')
-
-    @property
+    @safe_property
     def twitter(self):
         username_or_url = self.contact_detail_value('twitter') or \
             self.link_value('twitter')
@@ -111,7 +75,7 @@ class Person(PopoloObject):
             return extract_twitter_username(username_or_url)
         return None
 
-    @property
+    @safe_property
     def twitter_all(self):
         # The Twitter screen names in contact_details and links will
         # in most cases be the same, so remove duplicates:
@@ -120,32 +84,16 @@ class Person(PopoloObject):
             self.contact_detail_values('twitter') +
             self.link_values('twitter'))
 
-    @property
-    def phone(self):
-        return self.contact_detail_value('phone')
-
-    @property
-    def phone_all(self):
-        return self.contact_detail_values('phone')
-
-    @property
-    def facebook(self):
-        return self.link_value('facebook')
-
-    @property
-    def facebook_all(self):
-        return self.link_values('facebook')
-
-    @property
-    def fax(self):
-        return self.contact_detail_value('fax')
-
-    @property
-    def fax_all(self):
-        return self.contact_detail_values('fax')
-
-    def __repr__(self):
-        return self.repr_helper(self.name)
+    @safe_property
+    def memberships(self):
+        
+        from .collections import MembershipCollection
+        
+        membership_lists = [
+            m.data for m in self.all_popolo.memberships
+            if m.person_id == self.id
+        ]
+        return MembershipCollection(membership_lists,self.all_popolo)
 
     def name_at(self, particular_date):
         historic_names = [n for n in self.other_names if n.get('end_date')]
@@ -162,169 +110,52 @@ class Person(PopoloObject):
             raise Exception(msg.format(self, particular_date))
         return names_at_date[0]['name']
 
-    @property
-    def links(self):
-        return self.get_related_object_list('links')
-
-    @property
-    def contact_details(self):
-        return self.get_related_object_list('contact_details')
-
-    @property
-    def identifiers(self):
-        return self.get_related_object_list('identifiers')
-
-    @property
-    def images(self):
-        return self.get_related_object_list('images')
-
-    @property
-    def other_names(self):
-        return self.get_related_object_list('other_names')
-
-    @property
-    def sources(self):
-        return self.get_related_object_list('sources')
-
-    @property
-    def memberships(self):
-        from .collections import MembershipCollection
-        memberships_list = [
-            m.data for m in self.all_popolo.memberships
-            if m.person_id == self.id
-        ]
-        return MembershipCollection(memberships_list, self.all_popolo)
-
-    __hash__ = PopoloObject.__hash__
-
-
 class Organization(PopoloObject):
-
-    class DoesNotExist(ObjectDoesNotExist):
-        pass
-
-    class MultipleObjectsReturned(MultipleObjectsReturned):
-        pass
-
-    @property
-    def id(self):
-        return self.data.get('id')
-
-    @property
-    def name(self):
-        return self.data.get('name')
-
-    @property
-    def wikidata(self):
-        return self.identifier_value('wikidata')
-
-    @property
-    def classification(self):
-        return self.data.get('classification')
-
-    @property
-    def image(self):
-        return self.data.get('image')
-
-    @property
-    def founding_date(self):
-        return self.get_date('founding_date', None)
-
-    @property
-    def dissolution_date(self):
-        return self.get_date('dissolution_date', None)
-
-    @property
-    def seats(self):
-        return self.data.get('seats')
-
-    @property
-    def other_names(self):
-        return self.data.get('other_names', [])
-
-    def __repr__(self):
-        return self.repr_helper(self.name)
-
-    @property
-    def identifiers(self):
-        return self.get_related_object_list('identifiers')
-
-    @property
-    def links(self):
-        return self.get_related_object_list('links')
-
+    
+    id = Attribute()
+    name = Attribute()
+    wikidata = IdentiferAttribute()
+    classification = Attribute()
+    image = Attribute()
+    founding_date = DateAttribute(default=None)
+    dissolution_date = DateAttribute(default=None)
+    seats = Attribute()
+    other_names = Attribute(default=[])
+    identifiers = Attribute(default=[])
+    links = Attribute(default=[])
 
 class Membership(CurrentMixin, PopoloObject):
+    
+    role = Attribute()
+    person_id = Attribute()
+    organization_id = Attribute()
+    area_id = Attribute()
+    post_id = Attribute()
+    legislative_period_id = Attribute()
+    on_behalf_of_id = Attribute()
+    
+    person = RelatedAttribute()
+    organization = RelatedAttribute()
+    area = RelatedAttribute()
+    post = RelatedAttribute()
+    legislative_period = RelatedAttribute(collection="events")
+    on_behalf_of = RelatedAttribute(collection="organizations")
+    
+    start_date = DateAttribute(default=ApproxDate.PAST)
+    end_date = DateAttribute(default=ApproxDate.FUTURE)
 
-    class DoesNotExist(ObjectDoesNotExist):
-        pass
-
-    class MultipleObjectsReturned(MultipleObjectsReturned):
-        pass
-
-    @property
-    def role(self):
-        return self.data.get('role')
-
-    @property
-    def person_id(self):
-        return self.data.get('person_id')
-
-    @property
-    def person(self):
-        return self.all_popolo.persons.lookup_from_key[self.person_id]
-
-    @property
-    def organization_id(self):
-        return self.data.get('organization_id')
-
-    @property
-    def organization(self):
-        collection = self.all_popolo.organizations
-        return collection.lookup_from_key[self.organization_id]
-
-    @property
-    def area_id(self):
-        return self.data.get('area_id')
-
-    @property
-    def area(self):
-        return self.all_popolo.areas.lookup_from_key[self.area_id]
-
-    @property
-    def legislative_period_id(self):
-        return self.data.get('legislative_period_id')
-
-    @property
-    def legislative_period(self):
-        collection = self.all_popolo.events
-        return collection.lookup_from_key[self.legislative_period_id]
-
-    @property
-    def on_behalf_of_id(self):
-        return self.data.get('on_behalf_of_id')
-
-    @property
-    def on_behalf_of(self):
-        collection = self.all_popolo.organizations
-        return collection.lookup_from_key[self.on_behalf_of_id]
-
-    @property
-    def post_id(self):
-        return self.data.get('post_id')
-
-    @property
-    def post(self):
-        return self.all_popolo.posts.lookup_from_key[self.post_id]
-
-    @property
-    def start_date(self):
-        return self.get_date('start_date', ApproxDate.PAST)
-
-    @property
-    def end_date(self):
-        return self.get_date('end_date', ApproxDate.FUTURE)
-
+    @safe_property
+    def id(self):
+        """
+        returns a comparable id
+        """
+        return hash(str([self.person_id,
+                         self.organization_id,
+                         self.area_id,
+                         self.post_id,
+                         self.legislative_period_id,
+                         self.on_behalf_of_id]))
+    
     def __repr__(self):
         enclosed = u"'{0}' at '{1}'".format(
             self.person_id, self.organization_id)
@@ -340,108 +171,46 @@ class Membership(CurrentMixin, PopoloObject):
             return self.data != other.data
         return NotImplemented
 
-    @property
+    @safe_property
     def key_for_hash(self):
         return json.dumps(self.data, sort_keys=True)
-
+    
     def __hash__(self):
         return hash(self.key_for_hash)
 
 
 class Area(PopoloObject):
-
-    @property
-    def id(self):
-        return self.data.get('id')
-
-    @property
-    def name(self):
-        return self.data.get('name')
-
-    @property
-    def type(self):
-        return self.data.get('type')
-
-    @property
-    def identifiers(self):
-        return self.get_related_object_list('identifiers')
-
-    @property
-    def other_names(self):
-        return self.get_related_object_list('other_names')
-
-    @property
-    def wikidata(self):
-        return self.identifier_value('wikidata')
-
-    def __repr__(self):
-        return self.repr_helper(self.name)
+    
+    id = Attribute()
+    name = Attribute()
+    type = Attribute()
+    identifiers = Attribute(default=[])
+    other_names = Attribute(default=[])
+    wikidata = IdentiferAttribute()
 
 
 class Post(PopoloObject):
-
-    @property
-    def id(self):
-        return self.data.get('id')
-
-    @property
-    def label(self):
-        return self.data.get('label')
-
-    @property
-    def organization_id(self):
-        return self.data.get('organization_id')
-
-    @property
-    def organization(self):
-        collection = self.all_popolo.organizations
-        return collection.lookup_from_key[self.organization_id]
-
-    def __repr__(self):
-        return self.repr_helper(self.label)
+    
+    id = Attribute()
+    label = Attribute()
+    organization_id = Attribute()
+    organization = RelatedAttribute()
 
 
 class Event(CurrentMixin, PopoloObject):
-
-    @property
-    def id(self):
-        return self.data.get('id')
-
-    @property
-    def name(self):
-        return self.data.get('name')
-
-    @property
-    def classification(self):
-        return self.data.get('classification')
-
-    @property
-    def start_date(self):
-        return self.get_date('start_date', ApproxDate.PAST)
-
-    @property
-    def end_date(self):
-        return self.get_date('end_date', ApproxDate.FUTURE)
-
-    @property
-    def organization_id(self):
-        return self.data.get('organization_id')
-
-    @property
-    def organization(self):
-        collection = self.all_popolo.organizations
-        return collection.lookup_from_key[self.organization_id]
-
-    def __repr__(self):
-        return self.repr_helper(self.name)
-
-    @property
-    def identifiers(self):
-        return self.get_related_object_list('identifiers')
+    id = Attribute()
+    name = Attribute()
+    classification = Attribute()
+    start_date = DateAttribute(default=ApproxDate.PAST)
+    end_date = DateAttribute(default=ApproxDate.FUTURE)
+    organization_id = Attribute()
+    organization = RelatedAttribute()
+    identifiers = Attribute(default=[])
 
     @property
     def memberships(self):
         from .collections import MembershipCollection
+        
         memberships_list = [
             m.data for m in self.all_popolo.memberships
             if m.legislative_period_id == self.id
